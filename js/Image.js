4708001668794e91afc9735f514daf707e6a137f
Ahmad ALamili <ahmad.alamili@tieto.com>
2020-11-04 19:00:13 +0100

    Chaged image src

--- js/Image.js
+++ js/Image.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import Images from '../images/Ada_Lovelace_portrait.jpg'
 function Image() {
-    return (<img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg" alt="Girl in a jacket" width="500" height="600"/>)
+    return (<img src= {Images} alt="Girl in a jacket" width="400" height="600"/>)
 }
 
 export default Image
