ed7825e76d998be91a3e209328da24880b5082ee
Ahmad ALamili <ahmad.alamili@tieto.com>
2020-11-04 18:50:43 +0100

    uppgift1

---
+++ js/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from './Image'
+import About from './About'
+function App() {
+    let name =" Ada Lovelace"
+return (
+    <section>
+<h2>{name}</h2>
+<Image/>
+<About/>
+
+</section>
+)
+
+}
+
+export default App
---
+++ js/Image.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Image() {
+    return (<img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg" alt="Girl in a jacket" width="500" height="600"/>)
+}
+
+export default Image
